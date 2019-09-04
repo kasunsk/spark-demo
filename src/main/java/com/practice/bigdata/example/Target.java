7f3e0a8d9b8b1492fe4f11d9f7e5dace339f0479
Kasun Kariyawasam <kasunsk@gmail.com>
2019-09-04 22:57:19 +0800

    Practice

--- src/main/java/com/practice/bigdata/example/Target.java
+++ src/main/java/com/practice/bigdata/example/Target.java
@@ -1,6 +1,7 @@
 package com.practice.bigdata.example;
 
-import java.util.Comparator;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by kasun on 9/4/19.
@@ -9,7 +10,29 @@ public class Target {
 
     public static void main(String[] args) {
 
+        Integer target = 10;
+        Integer[] array = new Integer[]{1, 5, 6, 4, 7};
+
+        int[] arraySum = new int[2];
+
+        List<Integer> numbers = Arrays.asList(array);
+
+        boolean found = false;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (i != j && array[i] + array[j] == target) {
+                    arraySum[0] = i + 1;
+                    arraySum[1] = j + 1;
+                    found = true;
+                    break;
+                }
+                if (found) {
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < arraySum.length; i++) {
+            System.out.print(" " + arraySum[i]);
+        }
     }
-
-
 }
